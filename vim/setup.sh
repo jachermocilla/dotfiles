1e81f495271698aa93d536a19c2ae8ddcbe28d28
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2018-08-11 18:47:45 +0800

    added airline

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 #jachermocilla@gmail.com
 
+#install plugins using pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
 sudo apt-get install exuberant-ctags
 mkdir ~/.vim
 unzip -o taglist_46.zip -d ~/.vim/
 cp srg-license.txt ~/.vim/
 cp vimrc ~/.vimrc
+
+git clone --depth 1 https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 
