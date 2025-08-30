223d80794ef47cf02bba9c22c3860f622a4507be
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2025-08-30 12:06:30 +0800

    edits

--- terminator/setup.sh
+++ terminator/setup.sh
@@ -6,5 +6,5 @@ cp config ~/.config/terminator/config
 #install Monaco Font
 sudo mkdir -p /usr/share/fonts/truetype/ttf-monaco && \
 #sudo wget https://github.com/jachermocilla/dotfiles/blob/master/terminator/Monaco_Linux.ttf -O /tmp/Monaco_Linux.ttf && \
-sudo mv /tmp/Monaco_Linux.ttf /usr/share/fonts/truetype/ttf-monaco/Monaco_Linux.ttf && \
+sudo cp ./Monaco_Linux.ttf /usr/share/fonts/truetype/ttf-monaco/Monaco_Linux.ttf && \
 sudo fc-cache
