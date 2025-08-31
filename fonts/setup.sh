e2288b4d8c559be4edc1124113d2d9a712188427
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2025-08-31 16:31:35 +0800

    edits

--- fonts/setup.sh
+++ fonts/setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-mkdir -p ~/.config/terminator
-cp config ~/.config/terminator/config
+#ibm plex mono
+sudo apt install fonts-ibm-plex
 
 #install Monaco Font
 sudo mkdir -p /usr/share/fonts/truetype/ttf-monaco && \
-#sudo wget https://github.com/jachermocilla/dotfiles/blob/master/terminator/Monaco_Linux.ttf -O /tmp/Monaco_Linux.ttf && \
-sudo cp ./Monaco_Linux.ttf /usr/share/fonts/truetype/ttf-monaco/Monaco_Linux.ttf && \
+sudo cp ./Monaco_Linux.ttf /usr/share/fonts/truetype/ttf-monaco/Monaco_Linux.ttf
+
+
+sudo mkdir -p /usr/share/fonts/truetype/jetbrains && \
+sudo unzip -j JetBrainsMono-2.304.zip -d /usr/share/fonts/truetype/jetbrains/ 
+
 sudo fc-cache
